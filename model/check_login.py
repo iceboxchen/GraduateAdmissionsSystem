5399e8565a0cc0c93a9493e45d8e9d4240b874e6
dotorun <153896581+dotorun@users.noreply.github.com>
2024-12-22 02:07:44 +0800

    对曾的代码做了适配性修改（部分）

--- model/check_login.py
+++ model/check_login.py
@@ -7,7 +7,25 @@ def is_null(username,password):
 		return False
 
 
-def is_existed(username,password):
+def is_existed_teacher(username,password):
+	sql="SELECT * FROM teacherPasswords WHERE username ='%s' and password ='%s'" %(username,password)
+	cur.execute(sql)
+	result = cur.fetchall()
+	if (len(result) == 0):
+		return False
+	else:
+		return True
+
+def exist_user_teacher(username):
+	sql = "SELECT * FROM teacherpasswords WHERE username ='%s'" % (username)
+	cur.execute(sql)
+	result = cur.fetchall()
+	if (len(result) == 0):
+		return False
+	else:
+		return True
+
+def is_existed_student(username,password):
 	sql="SELECT * FROM studentPasswords WHERE username ='%s' and password ='%s'" %(username,password)
 	cur.execute(sql)
 	result = cur.fetchall()
@@ -16,8 +34,26 @@ def is_existed(username,password):
 	else:
 		return True
 
-def exist_user(username):
-	sql = "SELECT * FROM studentPasswords WHERE username ='%s'" % (username)
+def exist_user_student(username):
+	sql = "SELECT * FROM studentpasswords WHERE username ='%s'" % (username)
+	cur.execute(sql)
+	result = cur.fetchall()
+	if (len(result) == 0):
+		return False
+	else:
+		return True
+
+def is_existed_admin(username,password):
+	sql="SELECT * FROM admin WHERE username ='%s' and password ='%s'" %(username,password)
+	cur.execute(sql)
+	result = cur.fetchall()
+	if (len(result) == 0):
+		return False
+	else:
+		return True
+
+def exist_user_admin(username):
+	sql = "SELECT * FROM admin WHERE username ='%s'" % (username)
 	cur.execute(sql)
 	result = cur.fetchall()
 	if (len(result) == 0):
--- model/check_regist.py
+++ model/check_regist.py
@@ -2,11 +2,11 @@ from templates.config import conn
 
 cur = conn.cursor()
 
-def add_user(username, password):
+def add_user(username, password, studentID, studentName, studentIDnumber, studentPhone):
     # sql commands
-    sql = "INSERT INTO studentPasswords(username, password) VALUES ('%s','%s')" %(username, password)
+    sql = "INSERT INTO studentPasswords(username, password, studentID, studentName, studentIDnumber, studentPhone) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (username, password, studentID, studentName, studentIDnumber, studentPhone)
     # execute(sql)
     cur.execute(sql)
     # commit
     conn.commit()  # 对数据库内容有改变，需要commit()
-    conn.close()
+
